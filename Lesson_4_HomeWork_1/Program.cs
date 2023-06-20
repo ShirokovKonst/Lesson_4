0924085cd3b3a8f62357ab2918c0b0ba8b928a3d
Kostya <ashirokovkonstantin@gmail.com>
2023-06-20 17:25:46 +0500

    Create lesson 4 homework 1

---
+++ Lesson_4_HomeWork_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson_4_HomeWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Prompt (string message)
+            {
+                Console.WriteLine(message);
+                return int.Parse(Console.ReadLine());
+            }
+
+            int a = Prompt("Введите А: ");
+            int b = Prompt("Введите В: ");
+            //Не очень понятно из задания, зачем нужен цикл. 
+            //Задача решается через метод POW библиотеки Math. 
+            //На всякий случай, сделаю решение задачи и через цикл тоже.
+            //Console.WriteLine(Math.Pow(a,b));
+
+            int i = 0;
+            int result = 1;
+            while (i<b)
+            {
+                result = result*a;
+                i++;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
