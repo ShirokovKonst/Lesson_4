94e48e83fed7731b2a29fec70fe066f83879b485
Kostya <ashirokovkonstantin@gmail.com>
2023-06-20 17:34:02 +0500

    Create lesson 4 homework 2

---
+++ Lesson_4_HomeWork_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson_4_HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Prompt (string message)
+            {
+                Console.WriteLine(message);
+                return int.Parse(Console.ReadLine());
+            }
+
+            int num = Prompt("Введите число: ");
+            int result = 0;
+            int i = num;
+            while (i > 1)
+            {
+                result = result + i%10;
+                i=i/10;
+            }
+            Console.WriteLine("Сумма цифр в числе = " + result);
+        }
+    }
+}
