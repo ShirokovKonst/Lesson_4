0e937a27714e4a6f307b36f3cc88ecf641607c0c
Kostya <ashirokovkonstantin@gmail.com>
2023-06-21 19:26:59 +0500

    Create lesson 4 homrwork 3

---
+++ Lesson_4_HomeWork_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson_4_HomeWork_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[7];
+            int i = 0;
+            while (i < mas.Length)
+            {
+                mas[i] = new Random().Next(1, 100);
+                i++;
+            }
+            i=0;
+
+            while (i < mas.Length)
+            {
+                Console.WriteLine(mas[i]);
+                i++;
+            }
+        }
+    }
+}
